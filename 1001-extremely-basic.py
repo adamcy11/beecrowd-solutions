b1d8bffe49d6fc087527e938d383e604e2d060e2
Arthur <arthuras1@hotmail.com>
2026-04-22 20:26:54 -0300

    initial commit: add 1001 and 1002 solutions

---
+++ 1001-extremely-basic.py
@@ -0,0 +1,10 @@
+## 1001 - Extremely Basic
+
+
+A = int(input()) # int() converts the text from input() into an integer so we can do math
+B = int(input())
+X = A + B
+print(f"X = {X}") # f enables variable substitution inside {}
+
+
+
---
+++ 1002-area-of-a-circle.py
@@ -0,0 +1,6 @@
+## 1002 - Area of a Circle
+
+n = 3.14159
+R = float(input())
+A = n * R * R
+print(f"A={A:.4f}") # .4f formats the result to 4 decimal places
